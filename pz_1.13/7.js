b89e05c329e06ebd396afb638bad64654cd4dcbf
3VlaDIslaV3 <bublik@bezsalu.pp.ua>
2021-03-02 16:04:42 +0200

    pz_1.13.3

--- pz_1.13/7.js
+++ pz_1.13/7.js
@@ -1,17 +1,19 @@
 function count(obj) {
     let count = 0;
     for (let key in obj) {
+      
         count++
     }
+    
     return count;
 }
 
 let a = { a: 1, b: 2 };
-console.log(count(a)); // 2
+console.log(count(a)); 
 let b = function () {};
-console.log(count(b)); // 0
+console.log(count(b)); 
 let c = [1, 2, 3];
-console.log(count(c)); // 3
+console.log(count(c)); 
 let d = [];
 d[100] = 1;
-console.log(count(d)); // 1
+console.log(count(d)); 
