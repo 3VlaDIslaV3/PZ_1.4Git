5dfbf2097176cb861c5c0346ceb059305d292423
3VlaDIslaV3 <bublik@bezsalu.pp.ua>
2021-03-02 16:02:42 +0200

    pz_1.13.1

---
+++ pz_1.13/9.js
@@ -0,0 +1,21 @@
+//#9
+
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+    getSalary() { return console.log(`Salary worker: ${this.days * this.days}`) }
+}
+let w1 = new Worker('Vlad', 'Bublik', 4040, 15)
+let w2 = new Worker('Vlad', 'Kovalenko', 1500, 10)
+let w3 = new Worker('Alex', 'Kluchnic', 3400, 20)
+w1.getSalary();
+w2.getSalary();
+w3.getSalary();
+console.log(w1)
+console.log(w2)
+console.log(w3)
+
