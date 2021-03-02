42a1b0f7a731f015806f3115c7dc38eed3163cfd
3VlaDIslaV3 <bublik@bezsalu.pp.ua>
2021-03-02 16:18:10 +0200

    pz_1.13.9

---
+++ pz_1.13/4.js
@@ -0,0 +1,11 @@
+function createGreetable (str) {
+    const result = function (){
+    };
+    result.prototype.name = str;
+    result.prototype.greet = function (greeting){
+        return ${greeting}, ${result.prototype.name}!;
+    }
+    return result;
+}
+const g = createGreetable(Oleg);
+console.log (g.prototype.greet('Hello'));
